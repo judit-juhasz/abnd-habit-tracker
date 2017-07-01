31bbb67fae46959eab46a76373cb91b260f235f7
Judit Juhasz <judit-juhasz@users.noreply.github.com>
2017-07-01 23:39:34 +0200

    Use Contract class to define constants

---
+++ app/src/main/java/name/juhasz/judit/udacity/habittracker/data/HabitContract.java
@@ -0,0 +1,18 @@
+package name.juhasz.judit.udacity.habittracker.data;
+
+import android.provider.BaseColumns;
+
+public final class HabitContract {
+
+    private HabitContract() { }
+
+    public final static class HabitEntry implements BaseColumns {
+
+        public final static String HABITS_TABLE_NAME ="habits";
+
+        public final static String COLUMN_HABIT_NAME = "habit";
+        public final static String COLUMN_DATE_YEAR = "year";
+        public final static String COLUMN_DATE_MONTH = "month";
+        public final static String COLUMN_DATE_DAY = "day";
+    }
+}
